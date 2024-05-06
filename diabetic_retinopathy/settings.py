5e4d1b24f1702635e7793f59adc4ca39876221fa
Balacheran <125751467+Balacheran@users.noreply.github.com>
2024-05-06 18:13:10 +0530

    Update settings.py

--- diabetic_retinopathy/settings.py
+++ diabetic_retinopathy/settings.py
@@ -121,9 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 if not DEBUG:
     STATIC_ROOT = ''
-# STATIC_ROOT = os.path.join(BASE_DIR,'static/')
+
 STATICFILES_DIRS = [
+    STATIC_URL,
     os.path.join(BASE_DIR, 'static/'),
 ]
